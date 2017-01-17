2fd04f0901522ab6f6dc2d86bf59c298fa2e4a63
Vace <ocdo@qq.com>
2017-01-17 16:35:03 +0800

    format

--- dist/bus.js
+++ dist/bus.js
@@ -2,37 +2,37 @@
 
 module.exports = function bus(_this) {
 
-	var events = Object.create(null),
-	    map = _this || Object.create(null),
-	    make = function make(type) {
-		var _type = ('' + type).toLowerCase();
-		return events[_type] || (events[_type] = []);
-	};
+  var events = Object.create(null),
+      map = _this || Object.create(null),
+      make = function make(type) {
+    var _type = ('' + type).toLowerCase();
+    return events[_type] || (events[_type] = []);
+  };
 
-	map.on = function (type, handler) {
-		return make(type).push(handler), map;
-	};
+  map.on = function (type, handler) {
+    return make(type).push(handler), map;
+  };
 
-	map.once = function (type, handler) {
-		var _handle = function _handle(event) {
-			handler.call(map, event);
-			map.off(type, _handle);
-		};
-		return map.on(type, _handle);
-	};
+  map.once = function (type, handler) {
+    var _handle = function _handle(event) {
+      handler.call(map, event);
+      map.off(type, _handle);
+    };
+    return map.on(type, _handle);
+  };
 
-	map.off = function (type, handler) {
-		var arr = make(type),
-		    index = arr.indexOf(handler);
-		index !== -1 && arr.splice(index, 1);
-		return map;
-	};
+  map.off = function (type, handler) {
+    var arr = make(type),
+        index = arr.indexOf(handler);
+    index !== -1 && arr.splice(index, 1);
+    return map;
+  };
 
-	map.emit = function (type, event) {
-		return make('*').concat(make(type)).forEach(function (handler) {
-			return handler.call(map, event);
-		}), map;
-	};
+  map.emit = function (type, event) {
+    return make('*').concat(make(type)).forEach(function (handler) {
+      return handler.call(map, event);
+    }), map;
+  };
 
-	return map;
+  return map;
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-module.exports = function bus(_this){
+module.exports = function bus(_this) {
 
-	var events = Object.create(null),
+  var events = Object.create(null),
 
-		map = _this || Object.create(null),
+    map = _this || Object.create(null),
 
-		make = type => {
-			let _type = ('' + type).toLowerCase()
-			return events[ _type ] || ( events[ _type ] = [])
-		}
+    make = type => {
+      let _type = ('' + type).toLowerCase()
+      return events[_type] || (events[_type] = [])
+    }
 
-	map.on = (type,handler) => (make(type).push(handler),map)
+  map.on = (type, handler) => (make(type).push(handler), map)
 
-	map.once = (type,handler) => {
-		let _handle = event => {
-			handler.call(map,event)
-			map.off(type,_handle)
-		}
-		return map.on(type,_handle)
-	}
+  map.once = (type, handler) => {
+    let _handle = event => {
+      handler.call(map, event)
+      map.off(type, _handle)
+    }
+    return map.on(type, _handle)
+}
 
-	map.off = (type,handler) => {
-		var arr = make(type),
-			index = arr.indexOf(handler)
-		index !== -1 && arr.splice(index,1)
-		return map
-	}
+  map.off = (type, handler) => {
+    var arr = make(type),
+        index = arr.indexOf(handler)
+    index !== -1 && arr.splice(index, 1)
+    return map
+  }
 
-	map.emit = (type,event) => (make('*').concat(make(type)).forEach(handler => handler.call(map,event)),map)
+  map.emit = (type, event) => (make('*').concat(make(type)).forEach(handler => handler.call(map, event)), map)
 
-	return map
+  return map
 }
